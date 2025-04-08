995c2c15df23f01fe00b85c0bf5308bb023daf59
Kirill Pukhov <kirillpukhov2004@gmail.com>
2025-04-08 18:31:07 +0300

    Deletes unused code

--- EMSwiftUI/Source/MangaListScreen/Service/MangaListService.swift
+++ EMSwiftUI/Source/MangaListScreen/Service/MangaListService.swift
@@ -6,14 +6,11 @@
 //
 
 import Foundation
-import UIKit
 import Factory
 import Netify
 
 protocol MangaListService {
     func fetchManga() async throws -> MangaListModel
-
-    func fetchCover(for item: MangaListItem) async throws -> UIImage
 }
 
 enum MangaListServiceError: Error {
@@ -30,21 +27,5 @@ final class MangaListServiceImpl: MangaListService {
     
     func fetchManga() async throws -> MangaListModel {
         try await netify.request(API.mangaList, type: MangaListModel.self)
-    }
-
-    func fetchCover(for item: MangaListItem) async throws -> UIImage {
-        guard let url = item.coverURL else {
-            throw  MangaListServiceError.urlError
-        }
-
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = "GET"
-        let (data, _) = try await URLSession.shared.data(for: urlRequest)
-
-        guard let image = UIImage(data: data) else {
-            throw MangaListServiceError.imageDataDecodingError
-        }
-
-        return image
     }
 }
