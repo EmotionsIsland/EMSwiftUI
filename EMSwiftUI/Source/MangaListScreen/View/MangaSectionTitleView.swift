a81c9b038edb317e50df5840dcbe89a1bdd787a7
Ivanov Ilya <gergen892@gmail.com>
2025-06-23 13:11:12 +0500

    [+] - Создал вью для отображения манги по секциям (MangaSectionView) и вью для тайтла секции(SectionTitleView)

--- EMSwiftUI/Source/MangaListScreen/View/MangaSectionTitleView.swift
+++ EMSwiftUI/Source/MangaListScreen/View/MangaSectionTitleView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct MangaSectionTitleView: View {
+    let titleText: String
+    let action: () -> Void
+    
     var body: some View {
-        VStack {
-            // TODO: Create section title View
+        HStack {
+            Text(titleText)
+                .font(.custom("SF-Pro-Text-Bold", size: 20))
+            
+            Spacer()
+            
+            moreButton(action)
         }
     }
+}
+
+private extension MangaSectionTitleView {
+    func moreButton(_ action: @escaping () -> Void) -> some View {
+        return Button(action: action) {
+            HStack(spacing: 4) {
+                Text("more")
+                    .font(.system(size: 16))
+                    .foregroundColor(.black)
+                
+                Image(systemName: "chevron.right")
+                    .font(.system(size: 16, weight: .medium))
+                    .foregroundColor(.black)
+            }
+        }
+        .buttonStyle(.plain)
+    }
+}
+
+#Preview {
+    MangaSectionTitleView(titleText: "Popular") {}
 }
--- EMSwiftUI/Source/MangaListScreen/View/MangaSectionView.swift
+++ EMSwiftUI/Source/MangaListScreen/View/MangaSectionView.swift
@@ -7,10 +7,58 @@
 
 import SwiftUI
 
-struct MangaSectionView: View {
+struct MangaSectionView<VM: MangaListViewModel>: View {
+    @StateObject private var viewModel: VM
+    
+    let sectionName: String
+    let mangas: [MangaData]
+    
+    init(viewModel: VM,
+         sectionName: String,
+         mangas: [MangaData]) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+        self.sectionName = sectionName
+        self.mangas = mangas
+    }
+    
+    private let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 25),
+        GridItem(.flexible(), spacing: 25),
+        GridItem(.flexible(), spacing: 25)
+    ]
+    
     var body: some View {
         VStack {
-            // TODO: Create section View
+            MangaSectionTitleView(titleText: sectionName) {
+            }
+            .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
+            
+            LazyVGrid(columns: columns, spacing: 25) {
+                ForEach(mangas, id: \.id) { manga in
+                    MangaSingleGridView(
+                        imageURL: viewModel.getMangaCoverURL(for: manga, .size512),
+                        title: manga.attributes.title.en ?? "",
+                        rating: 4.2,
+                        genres: genresString(for: manga))
+                }
+            }
         }
+        .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
+    }
+}
+
+private extension MangaSectionView {
+//    func coverURL(for manga: MangaData) -> String {
+//        let baseURL = "https://uploads.mangadex.org/covers/"
+//        if let cover = manga.relationships.first(where: { $0.type == "cover_art" }),
+//           let fileName = cover.attributes?.fileName {
+//            return baseURL + manga.id + "/" + fileName
+//        }
+//        return "https://via.placeholder.com/300x450.png?text=No+Image"
+//    }
+
+    func genresString(for manga: MangaData) -> String {
+        let genres = manga.attributes.tags.map { $0.attributes.name.en ?? "" }.joined(separator: ", ")
+        return genres.isEmpty ? "Unknown" : genres
     }
 }
