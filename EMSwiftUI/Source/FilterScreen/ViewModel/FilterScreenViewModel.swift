a02c43c0bac841a718b2c3dc3f8106a0771e3818
nobu1437 <omabrirdu@yandex.ru>
2025-12-18 11:32:27 +0500

    поправил отступы, исправил ошибку с потоками

--- EMSwiftUI/Source/FilterScreen/ViewModel/FilterScreenViewModel.swift
+++ EMSwiftUI/Source/FilterScreen/ViewModel/FilterScreenViewModel.swift
@@ -9,20 +9,20 @@ final class FilterScreenViewModel: ObservableObject {
     private let service: FilterListService
     
     var filterSelectionModel: FilterSelectionModel {
-         return FilterSelectionModel(
-             category: category.first ?? "Selection",
-             selectedTags: tagsSelected,
-             onTagSelect: { [weak self] tag in self?.changeStateTag(for: tag) },
-             onReset: { [weak self] in self?.resetTags() }
-         )
-     }
-     
-     let navigationTitle = "Filters"
-     
+        return FilterSelectionModel(
+            category: category.first ?? "Selection",
+            selectedTags: tagsSelected,
+            onTagSelect: { [weak self] tag in self?.changeStateTag(for: tag) },
+            onReset: { [weak self] in self?.resetTags() }
+        )
+    }
+    
+    let navigationTitle = "Filters"
+    
     init(service: FilterListService) {
-         self.service = service
-     }
-     
+        self.service = service
+    }
+    
     @MainActor
     func getTags() async {
         self.isLoading = true
@@ -30,37 +30,38 @@ final class FilterScreenViewModel: ObservableObject {
             let tag = try await service.getTags()
             await getCategory(from: tag)
             self.tag = tag
-            self.isLoading = false
         } catch {
-            self.isLoading = false
             print(error)
         }
+        self.isLoading = false
     }
-     
+    
+    @MainActor
     private func getCategory(from tag: FilterTagModel) async {
-         var result: [String] = []
-         var seen: Set<String> = []
-         
-         for item in tag.data {
-             let category = item.attributes.group
-             let formattedCategory = category.prefix(1).uppercased() + category.dropFirst()
-             
-             if !seen.contains(formattedCategory) {
-                 result.append(formattedCategory)
-                 seen.insert(formattedCategory)
-             }
-         }
-         
-         self.category += result
-     }
-
-     private func filterCategory(_ category: String) -> [Tag] {
-         return tag?.data.filter { $0.attributes.group == category.lowercased() } ?? []
-     }
-     
-     private func resetTags() {
-         tagsSelected = []
-     }
+        var result: [String] = []
+        var seen: Set<String> = []
+        
+        for item in tag.data {
+            let category = item.attributes.group
+            let formattedCategory = category.prefix(1).uppercased() + category.dropFirst()
+            
+            if !seen.contains(formattedCategory) {
+                result.append(formattedCategory)
+                seen.insert(formattedCategory)
+            }
+        }
+        
+        self.category += result
+    }
+    
+    private func filterCategory(_ category: String) -> [Tag] {
+        return tag?.data.filter { $0.attributes.group == category.lowercased() } ?? []
+    }
+    
+    private func resetTags() {
+        tagsSelected = []
+    }
+    
     private func changeStateTag(for tag: Tag) {
         if let index = tagsSelected.firstIndex(where: { $0.id == tag.id }) {
             self.tagsSelected.remove(at: index)
@@ -78,7 +79,7 @@ final class FilterScreenViewModel: ObservableObject {
         let selectedStates = tags.map { tag in
             tagsSelected.contains(where: { $0.id == tag.id })
         }
-
+        
         return FilterSectionModel(
             category: category,
             tags: tags,
