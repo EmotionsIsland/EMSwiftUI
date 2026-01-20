a6a7f763f8b38a833a22b2d9c2c2b82d13496a6c
nobuuu1 <158583685+nobuuu1@users.noreply.github.com>
2026-01-20 10:38:34 +0500

    подправил спейсинг

--- EMSwiftUI/Source/MangaListScreen/Model/RatingConstants.swift
+++ EMSwiftUI/Source/MangaListScreen/Model/RatingConstants.swift
@@ -4,5 +4,5 @@ enum Constants {
     static let maxRating: Int = 5
     static let ratingWidth: CGFloat = 17
     static let ratingHeight: CGFloat = 16
-    static let ratingSpacing: CGFloat = 4
+    static let ratingSpacing: CGFloat = (100 - (ratingWidth * 5)) / 4
 }
