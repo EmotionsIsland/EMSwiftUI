f65eca8a9f73a26a8935c23163a4381baedb0eba
Denis <bluesky171@mail.ru>
2026-03-10 21:43:48 +0300

    refactor MangaListViewModel

--- EMSwiftUI/Source/MangaListScreen/ViewModel/MangaListViewModel.swift
+++ EMSwiftUI/Source/MangaListScreen/ViewModel/MangaListViewModel.swift
@@ -14,12 +14,10 @@ enum SizeFormat: String {
     case size1024 = ".1024.jpg"
 }
 
+@MainActor
 protocol MangaListViewModel: ObservableObject {
     var items: [MangaData] { get }
     var screenState: MangaListViewState { get }
-    var popular: [MangaData] { get }
-    var recentlyAdded: [MangaData] { get }
-    var lastUpdates: [MangaData] { get }
     
     var popularVM: [MangaGridItemViewModel] { get }
     var recentlyAddedVM: [MangaGridItemViewModel] { get }
@@ -28,12 +26,10 @@ protocol MangaListViewModel: ObservableObject {
     func getData() async
 }
 
+@MainActor
 final class MangaListViewModelImpl: MangaListViewModel {
     @Published var items: [MangaData] = []
     @Published var screenState: MangaListViewState = .isLoading
-    @Published private(set) var popular: [MangaData] = []
-    @Published private(set) var recentlyAdded: [MangaData] = []
-    @Published private(set) var lastUpdates: [MangaData] = []
 
     @Published var popularVM: [MangaGridItemViewModel] = []
     @Published var recentlyAddedVM: [MangaGridItemViewModel] = []
@@ -45,7 +41,7 @@ final class MangaListViewModelImpl: MangaListViewModel {
         self.service = service
     }
 
-    @MainActor func getData() async {
+    func getData() async {
         do {
             async let popularResponse = service.getManga(sort: .popular)
             async let recentlyAddedResponse = service.getManga(sort: .recentlyAdded)
@@ -53,13 +49,9 @@ final class MangaListViewModelImpl: MangaListViewModel {
 
             let (popularModel, recentlyAddedModel, lastUpdatesModel) = try await (popularResponse, recentlyAddedResponse, lastUpdatesResponse)
 
-            popular = popularModel.data
-            recentlyAdded = recentlyAddedModel.data
-            lastUpdates = lastUpdatesModel.data
-
-            popularVM = popular.map { MangaGridItemViewModel(manga: $0) }
-            recentlyAddedVM = recentlyAdded.map { MangaGridItemViewModel(manga: $0) }
-            lastUpdatesVM = lastUpdates.map { MangaGridItemViewModel(manga: $0) }
+            popularVM = popularModel.data.map { MangaGridItemViewModel(manga: $0) }
+            recentlyAddedVM = recentlyAddedModel.data.map { MangaGridItemViewModel(manga: $0) }
+            lastUpdatesVM = lastUpdatesModel.data.map { MangaGridItemViewModel(manga: $0) }
 
             screenState = .isLoaded
         } catch {
